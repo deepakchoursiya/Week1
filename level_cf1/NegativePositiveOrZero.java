510862d85d4aee6bad7779f0d1247ff37da265f9
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-10 19:34:19 +0530

    Problem's Solution

---
+++ level_cf1/NegativePositiveOrZero.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class NegativePositiveOrZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from the user
+        System.out.print("Enter a number - ");
+        int number = sc.nextInt();
+
+        // Checking if the number is positive, negative, or zero
+        if (number > 0) {
+            System.out.println("The number is positive");
+        } else if (number < 0) {
+            System.out.println("The number is negative");
+        } else {
+            System.out.println("The number is zero");
+        }
+    }
+}
