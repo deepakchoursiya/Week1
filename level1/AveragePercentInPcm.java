8d89357a61db9b9c44fa46b7a5fc192d3726aa25
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-10 15:59:35 +0530

    percetage of pcm

---
+++ level1/AveragePercentInPcm.java
@@ -0,0 +1,18 @@
+class AveragePercentInPcm
+{
+	public static void main(String args[])
+	{
+		int mathsMarks = 94;
+		// marks scored in maths
+		
+		int physicsMarks = 95;
+		// marks scored in physics
+		
+		int chemistryMarks = 96;
+		// marks scored in chemistry
+		
+		double average = ( mathsMarks + physicsMarks + chemistryMarks )/3 ;
+		
+		System.out.println(" Sam’s average mark in PCM is  " + average);
+	}
+}
