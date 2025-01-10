bc768a0695faea4660c27ae2290d5a9940f29ec2
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-10 16:07:06 +0530

    Problem Solution

---
+++ level1/FindAge.java
@@ -0,0 +1,18 @@
+class FindAge
+{	
+	public static void main(String []args)
+		{
+			int birthYear = 2000 ;
+			//birth year of Harry
+			
+			int currentYear = 2024;	
+			//current Year
+			
+			int age = currentYear - birthYear;
+			//age calculation	
+			
+			System.out.println("Harry's age is " + age);
+			
+		}
+}
+
