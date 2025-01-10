e8e7cf21cd8987270eaf6eba32e7a0a65cd6166a
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-10 16:10:50 +0530

    Problem Solution

---
+++ level1/PerimeterOfSquare.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class PerimeterOfSquare
+{
+	public static void main(String args[])
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int sideOfSquare = input.nextInt();
+		// side Of Square
+		
+		int perimeterOfSquare = 4 * sideOfSquare;
+		//perimeter Of Square
+		
+		System.out.println(" The length of the side is " + sideOfSquare + " whose perimeter is " + perimeterOfSquare);
+		
+		
+	
+		
+	}
+}
