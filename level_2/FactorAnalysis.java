904c94c0dd35bf2a78cff7895f43e45a74f67b65
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-11 15:10:30 +0530

    Program added of factor analysis

---
+++ level_2/FactorAnalysis.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class FactorAnalysis {
+
+    // Method to find factors and return as an array
+    public static int[] findFactors(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+        return factors;
+    }
+
+    // Method to find the sum of factors
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += factor;
+        }
+        return sum;
+    }
+
+    // Method to find the product of factors
+    public static long productOfFactors(int[] factors) {
+        long product = 1;
+        for (int factor : factors) {
+            product *= factor;
+        }
+        return product;
+    }
+
+    // Method to find the sum of squares of factors
+    public static int sumOfSquares(int[] factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += Math.pow(factor, 2);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input number
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Find factors
+        int[] factors = findFactors(number);
+
+        // Display results
+        System.out.println("Factors: " + Arrays.toString(factors));
+        System.out.println("Sum of factors: " + sumOfFactors(factors));
+        System.out.println("Product of factors: " + productOfFactors(factors));
+        System.out.println("Sum of squares of factors: " + sumOfSquares(factors));
+    }
+}
